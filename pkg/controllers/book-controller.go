9c93378fdc6c1b8f8cb90358a38e45bfafaa801c
Harshit Katiyar2 <harshitk2@chetu.com>
2025-01-10 23:25:25 +0530

    Added 2 controller functions

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/itsharshitk/go-bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
